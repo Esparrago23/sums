93f9358eb2abe81eea65e6df3c6aa73da9e4f48b
vhag233392 <233392@ids.upchiapas.edu.mx>
2025-05-04 11:28:11 -0600

    Adaptacion de entrevistador

--- src/Entrevistador/domain/entities/entrevistador.ts
+++ src/Entrevistador/domain/entities/entrevistador.ts
@@ -1,9 +1,11 @@
+// src/Entrevistador/domain/entities/entrevistador.ts
 export class Entrevistador {
     constructor(
         public id: number,
         public nombre: string,
         public fecha_registro: Date,
         public unidad_salud_id: number,
-        public datos_laborales_id: number
+        public datos_laborales_id: number,
+        public rol_id: number  // Nuevo campo para asociar con la tabla rol
     ) {}
 }
